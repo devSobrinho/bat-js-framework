b84ad2e95f9430c993f54fde0b5efb9671ea9d0a
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-15 13:54:00 -0300

    feat: Add RouteHandlerConfigurator class for configuring route handlers

---
+++ src/configs/route-handler-configurator.config.ts
@@ -0,0 +1,48 @@
+import "reflect-metadata";
+
+export type TExecAppMethods = "get" | "post" | "put" | "patch" | "delete";
+
+import { Request, Response } from "express";
+import { setDataControllerMapper } from "../metadatas/router.metada";
+
+export class RouteHandlerConfigurator {
+  constructor(
+    protected target: any,
+    protected propertyKey: string,
+    protected descriptor: PropertyDescriptor,
+    protected path: string,
+    protected type: TExecAppMethods = "get"
+  ) {}
+
+  exec() {
+    this.formatPath();
+    setDataControllerMapper(this.target.constructor, {
+      path: this.path,
+      target: this.target,
+      propertyKey: this.propertyKey,
+      type: this.type,
+    });
+    this.descriptor.value = this.cbResponse;
+  }
+
+  private formatPath() {
+    return this.path.startsWith("/") ? this.path.slice(1) : this.path;
+  }
+
+  private async cbResponse(req: Request, res: Response) {
+    const originalMethod = this.descriptor.value;
+
+    // Chama a função original e captura o resultado
+    const result = await originalMethod.call(this, req, res);
+    if (!result) {
+    } else if (typeof result === "string") {
+      // Verifica o tipo do resultado e envia a resposta adequada
+      res.send(result); // Envia como string
+    } else if (typeof result === "object") {
+      res.json(result); // Envia como JSON
+    } else {
+      // Se o tipo não for suportado, envia um erro 500
+      res.status(500).send("Internal Server Error");
+    }
+  }
+}
