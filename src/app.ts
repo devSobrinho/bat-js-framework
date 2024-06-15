7470c6a42f0001671d7c4b4bb38414856f672507
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-15 14:05:54 -0300

    feat: Created app server

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import { ExpressLoader } from "./loaders/express.loader";
+
+dotenv.config();
+
+export const app = express();
+
+ExpressLoader.init();
+
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server is running on port " + process.env.APP_PORT);
+});
