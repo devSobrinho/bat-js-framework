a163efcd43ee2dc65e5469113364e6c2e2b58713
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-15 22:07:42 -0300

    feat: Update controller decorator to use initControllerMapper function

--- src/common/decorators/controller.decorator.ts
+++ src/common/decorators/controller.decorator.ts
@@ -1,17 +1,14 @@
 import "reflect-metadata";
 
-import { getDataControllerMapper } from "../../metadatas/router.metada";
+import { initControllerMapper } from "../../metadatas/router.metada";
 
 // Decorator para definir o prefixo do controlador
 export function Controller(path: string = "/") {
   if (!path.endsWith("/")) path += "/";
   console.info(`@Controller('${path}')`);
   // Armazena informações sobre o controlador usando Reflect.defineMetadata
-  return function (
-    target: any,
-    propertyKey?: string,
-    descriptor?: PropertyDescriptor
-  ) {
-    getDataControllerMapper(target);
+  return function (target: any) {
+    // target.path = path;
+    initControllerMapper(target, path);
   };
 }
