6b49647c733105b7489ed68a937218cc530827f0
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-16 23:01:20 -0300

    feat: Add deleteC route handler to ExampleController

--- src/examples/controllers/example.controller.ts
+++ src/examples/controllers/example.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from "@Common/decorators/controller.decorator";
+import { Delete } from "@Common/decorators/delete.decorator";
 import { Get } from "@Common/decorators/get.decorator";
 import { Post } from "@Common/decorators/post.decorator";
+import { Query } from "@Common/decorators/query.decorator";
 import { Req } from "@Common/decorators/req.decorator";
 import { Res } from "@Common/decorators/res.decorator";
 import { Request, Response } from "express";
@@ -8,12 +10,17 @@ import { Request, Response } from "express";
 @Controller("/example")
 export class ExampleController {
   @Get("a/:id")
-  getA() {
-    return { message: "Hello World" };
+  getA(@Query() query: any) {
+    return { message: "Hello World", query };
   }
 
   @Post("b")
   getB(@Res() res: Response, @Req() req: Request) {
     res.json({ data: "ExampleController getB" });
+  }
+
+  @Delete("c")
+  deleteC(@Res() res: Response, @Req() req: Request) {
+    return "ExampleController deleteC";
   }
 }
