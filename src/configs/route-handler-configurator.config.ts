6e5a1ae61866a12576e6d4afd6ca23f39e25b277
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-16 22:01:46 -0300

    feat: Refactor route handler to include original method in callback response

--- src/configs/route-handler-configurator.config.ts
+++ src/configs/route-handler-configurator.config.ts
@@ -1,9 +1,8 @@
 import "reflect-metadata";
 
-export type TExecAppMethods = "get" | "post" | "put" | "patch" | "delete";
-
-import { Request, Response } from "express";
 import { setDataControllerMapper } from "../metadatas/router.metada";
+
+export type TExecAppMethods = "get" | "post" | "put" | "patch" | "delete";
 
 export class RouteHandlerConfigurator {
   constructor(
@@ -22,27 +21,9 @@ export class RouteHandlerConfigurator {
       propertyKey: this.propertyKey,
       type: this.type,
     });
-    const originalMethod = this.descriptor.value;
-    this.descriptor.value = async (req: Request, res: Response) =>
-      this.cbResponse(req, res, originalMethod);
   }
 
   private formatPath() {
     return this.path.startsWith("/") ? this.path.slice(1) : this.path;
-  }
-
-  private async cbResponse(req: Request, res: Response, originalMethod: any) {
-    // Chama a função original e captura o resultado
-    const result = await originalMethod.call(this, req, res);
-    if (!result) {
-    } else if (typeof result === "string") {
-      // Verifica o tipo do resultado e envia a resposta adequada
-      res.send(result); // Envia como string
-    } else if (typeof result === "object") {
-      res.json(result); // Envia como JSON
-    } else {
-      // Se o tipo não for suportado, envia um erro 500
-      res.status(500).send("Internal Server Error");
-    }
   }
 }
