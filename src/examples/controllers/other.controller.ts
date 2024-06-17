c08f467dc7f67b9e6aba15cc01323c4e60a39412
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-16 23:01:30 -0300

    feat: Add CRUD routes to OtherController

--- src/examples/controllers/other.controller.ts
+++ src/examples/controllers/other.controller.ts
@@ -1,18 +1,47 @@
 import { Controller } from "@Common/decorators/controller.decorator";
+import { Delete } from "@Common/decorators/delete.decorator";
 import { Get } from "@Common/decorators/get.decorator";
+import { Patch } from "@Common/decorators/patch.decorator";
+import { Post } from "@Common/decorators/post.decorator";
+import { Put } from "@Common/decorators/put.decorator";
 import { Req } from "@Common/decorators/req.decorator";
 import { Res } from "@Common/decorators/res.decorator";
 import { Request, Response } from "express";
 
 @Controller("/other")
 export class OtherController {
-  @Get("c/:id")
-  getC() {
-    return "Hello World";
+  @Get("/")
+  get(@Req() req: Request, @Res() res: Response) {
+    res.json({ data: "OtherController get" });
   }
 
-  @Get("d")
-  getD(@Req() req: Request, @Res() res: Response) {
-    res.json({ data: "OtherController getD" });
+  @Get("/:id")
+  getId() {
+    return "OtherController getId";
+  }
+
+  @Post("/")
+  post() {
+    return { data: "OtherController post" };
+  }
+
+  @Patch()
+  patch() {
+    return { data: "OtherController patch" };
+  }
+
+  @Put()
+  put() {
+    return { data: "OtherController put" };
+  }
+
+  @Delete()
+  delete() {
+    return { data: "OtherController delete" };
+  }
+
+  @Get("a")
+  getA(@Req() req: Request, @Res() res: Response) {
+    res.json({ data: "OtherController getA" });
   }
 }
