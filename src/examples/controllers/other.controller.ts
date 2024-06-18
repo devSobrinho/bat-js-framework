e911d5d026b2e49e047e43db764ac8a880948836
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-18 20:48:01 -0300

    feat: Add Param decorator for injecting request parameters

--- src/examples/controllers/other.controller.ts
+++ src/examples/controllers/other.controller.ts
@@ -1,9 +1,12 @@
+import { Body } from "@Common/decorators/body.decorator";
 import { Controller } from "@Common/decorators/controller.decorator";
 import { Delete } from "@Common/decorators/delete.decorator";
 import { Get } from "@Common/decorators/get.decorator";
+import { Param } from "@Common/decorators/param.decorator";
 import { Patch } from "@Common/decorators/patch.decorator";
 import { Post } from "@Common/decorators/post.decorator";
 import { Put } from "@Common/decorators/put.decorator";
+import { Query } from "@Common/decorators/query.decorator";
 import { Req } from "@Common/decorators/req.decorator";
 import { Res } from "@Common/decorators/res.decorator";
 import { Request, Response } from "express";
@@ -15,14 +18,20 @@ export class OtherController {
     res.json({ data: "OtherController get" });
   }
 
-  @Get("/:id")
-  getId() {
-    return "OtherController getId";
+  @Get("/:id/:name")
+  getId(
+    @Param("id") id: string,
+    @Param("name") name: string,
+    @Param() params: string,
+    @Query() query: any,
+    @Query("example") example: any
+  ) {
+    return { data: "OtherController getId", id, name, params, query, example };
   }
 
   @Post("/")
-  post() {
-    return { data: "OtherController post" };
+  post(@Body() body: any, @Body("example") example: any) {
+    return { data: "OtherController post", body, example };
   }
 
   @Patch()
