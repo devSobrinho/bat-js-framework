eb80b1417091638a9aa76c8bc06da8bf3b55bf75
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-18 20:35:23 -0300

    feat: Add Param decorator for injecting request parameters

---
+++ src/common/decorators/param.decorator.ts
@@ -0,0 +1,22 @@
+import {
+  generationArgControllerKeyMetadata,
+  setArgControllerMetadata,
+} from "metadatas/controller-arg.metadata";
+
+export const PARAM_PREFIX_KEY = Symbol("express:param:");
+
+// Decorator para injetar parametros da requisição
+export function Param(name?: string) {
+  return function (
+    target: any,
+    propertyKey: string | symbol,
+    parameterIndex: number
+  ) {
+    const metadataKey = generationArgControllerKeyMetadata(
+      PARAM_PREFIX_KEY,
+      propertyKey.toString(),
+      target
+    );
+    setArgControllerMetadata(metadataKey, parameterIndex, target, { name });
+  };
+}
