af7bbf89425038e2c434b4d62fc73637c01d0890
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-18 20:47:49 -0300

    feat: Add dependency injection to ExampleController constructor

--- src/examples/controllers/example.controller.ts
+++ src/examples/controllers/example.controller.ts
@@ -1,14 +1,23 @@
 import { Controller } from "@Common/decorators/controller.decorator";
 import { Delete } from "@Common/decorators/delete.decorator";
 import { Get } from "@Common/decorators/get.decorator";
+import { Inject } from "@Common/decorators/inject.decorator";
 import { Post } from "@Common/decorators/post.decorator";
 import { Query } from "@Common/decorators/query.decorator";
 import { Req } from "@Common/decorators/req.decorator";
 import { Res } from "@Common/decorators/res.decorator";
+import { ExampleService } from "examples/services/example.service";
 import { Request, Response } from "express";
 
 @Controller("/example")
 export class ExampleController {
+  constructor(
+    @Inject(ExampleService)
+    public exampleService: ExampleService
+  ) {
+    console.log("ExampleController constructor", this.exampleService);
+  }
+
   @Get("a/:id")
   getA(@Query() query: any) {
     return { message: "Hello World", query };
@@ -16,11 +25,15 @@ export class ExampleController {
 
   @Post("b")
   getB(@Res() res: Response, @Req() req: Request) {
-    res.json({ data: "ExampleController getB" });
+    this.getB(res, req);
+  }
+
+  getBas(...args: any[]) {
+    console.log(args[0], args[1]);
   }
 
   @Delete("c")
   deleteC(@Res() res: Response, @Req() req: Request) {
-    return "ExampleController deleteC";
+    return this.exampleService.deleteC(res, req);
   }
 }
