332f581dc9f276f323cc4d231a57b92c6371fddf
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-19 00:20:51 -0300

    feat: Add ExampleRepository and Connection classes for managing example-related data and database connection

---
+++ src/examples/repositories/example.repository.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@Common/decorators/injectable.decorator";
+
+@Injectable()
+export class ExampleRepository {
+  // constructor(public connection: Connection) {}
+
+  get(data: any) {
+    return {
+      message: "ExampleRepository get",
+      data,
+      // connection: this.connection.getRepository(),
+    };
+  }
+
+  post() {
+    return "ExampleRepository post";
+  }
+
+  put() {
+    return "ExampleRepository put";
+  }
+
+  delete() {
+    return "ExampleRepository delete";
+  }
+}
+
+@Injectable()
+export class Connection {
+  constructor() {
+    // console.log("Connection constructor");
+  }
+
+  connect() {
+    return "Connection connect";
+  }
+
+  disconnect() {
+    return "Connection disconnect";
+  }
+
+  getRepository() {
+    return "Connection getRepository";
+  }
+}
