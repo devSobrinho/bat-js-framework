6e1688fa275883634a916219d9fe2a6ddd31f897
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-19 00:58:15 -0300

    Refactor Inject decorator to remove unused code

--- src/common/decorators/inject.decorator.ts
+++ src/common/decorators/inject.decorator.ts
@@ -3,7 +3,6 @@ import "reflect-metadata";
 export function Inject(dependency?: any) {
   console.info(`@Inject() INIT`);
   return function (target: any, propertyKey: any, parameterIndex: number) {
-    // setDependencyTargetMapper(target);
     if (!target.constructor.inject) {
       target.constructor.inject = {};
     }
