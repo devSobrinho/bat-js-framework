65182e53de7ea0e53955d8e8697764947789fe38
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-06-19 20:23:47 -0300

    feat: Register controller in dependency container in Controller decorator

--- src/common/decorators/controller.decorator.ts
+++ src/common/decorators/controller.decorator.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
 
+import { dependencyContainerInstance } from "@Core/dependencies/container.dependency";
+
 import { initControllerMapper } from "../../metadatas/router.metadata";
 
 // Decorator para definir o prefixo do controlador
@@ -9,5 +11,6 @@ export function Controller(path: string = "/") {
   // Armazena informações sobre o controlador usando Reflect.defineMetadata
   return function (target: any) {
     initControllerMapper(target, path);
+    dependencyContainerInstance.register(target, target);
   };
 }
